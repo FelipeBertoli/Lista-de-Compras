4031ac294668a61982aedc8b5a807c9423c2f7cd
Laboratório Informática <labinfo@unifil.br>
2022-05-18 20:32:58 -0300

    V.1.1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  variavel = ' ';
+  lista = [{ codigo: 1, nome: 'Batata', feito: false },
+  { codigo: 2, nome: 'Cebola', feito: false },
+  { codigo: 3, nome: 'Tomate', feito: false }];
+
+
+  criarCodigo() {
+    var aux = 0;
+    this.lista.forEach(function (valor) {
+      if (valor.codigo > aux) aux = valor.codigo;
+      console.log(aux);
+    });
+    aux = aux + 1
+    return aux
+  }
+
+  adicionar() {
+
+    this.lista.push({ codigo: this.criarCodigo(), nome: this.variavel, feito: false });
+  }
+  remover(codigo) {
+    this.lista.splice(0,1);
+  }
+  constructor() { }
 }
